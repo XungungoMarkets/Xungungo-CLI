680f036e9f0b192dbab32c65f94f955642d30ca6
Fiambre <soy@fiambre.dev>
2026-03-12 21:48:44 -0300

    chore: Configure automatic version injection from Git tags

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Version is set at build time using -ldflags
-var Version = "0.2.0"
+var Version = "dev"
 
 // JSONOutput is a global flag to control JSON output format
 var JSONOutput bool
